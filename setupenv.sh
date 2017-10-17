35733aaa4eba15a9f13ce6ffba25a7a2cc4428e1
samambaman <felipe.gomes.ferreira@gmail.com>
2017-10-17 17:39:50 -0200

    adicao da instalacao do virtualenv

--- setupenv.sh
+++ setupenv.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-sudo apt-get install git vim
+sudo apt-get install git vim python3-pip
 git clone --depth=1 https://github.com/Bash-it/bash-it.git ~/.bash_it
 ~/.bash_it/install.sh
-sed -i -e 's/bobby/powerline/g' .bashrc
+sed -i -e 's/bobby/powerline/g' ~/.bashrc
+sudo pip3 install virtualenvwrapper 
+
